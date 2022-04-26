d2182ef8d3c14b77caebd16def2a5074e1a6e87b
Owner <leshagl@yandex.ru>
2022-04-26 10:42:59 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="App">
+<h1>Todo aplication</h1>
+<hr>
+<TodoList v-bind:todos="todos"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from '@/components/TodoList'
 export default {
   name: 'App',
+  data(){
+    return {
+      todos: [
+        {id: 1, title: 'Купить хлеб', completed: false},
+        {id: 2, title: 'Купить масло', completed: false},
+        {id: 3, title: 'Купить пиво', completed: false}
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    TodoList
+    
   }
 }
 </script>
